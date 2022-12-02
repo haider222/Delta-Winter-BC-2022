6e92f1d72656ddabf8103a3be28112311bf4e25a
Inga <varlinga@gmail.com>
2022-12-02 12:14:22 +0200

    Commit 18 sub-task after merge conflict resolved (hopefully)

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -6,12 +6,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/13SearchBoxVisibility.feature",
+        features =
+               {"src/test/resources/features/13SearchBoxVisibility.feature",
                 "src/test/resources/features/14SearchByNameRegexp.feature",
                 "src/test/resources/features/15SearchByDescription.feature",
                 "src/test/resources/features/17SearchedProductsCanBeSorted.feature",
                 "src/test/resources/features/19UserIsAbleToWriteReviewAfterPurchase.feature",
-                "src/test/resources/features/20ProductPageCompare.feature",},
+                "src/test/resources/features/20ProductPageCompare.feature",
+                "src/test/resources/features/18AbleToOpenProductPage&AddProductToCart.feature"},
+
         plugin = {"pretty", "html:cucumber-report/html-report.html",
                 "junit:cucumber-report/junit-report.xml",
                 "json:cucumber-report/json-report.json"},
